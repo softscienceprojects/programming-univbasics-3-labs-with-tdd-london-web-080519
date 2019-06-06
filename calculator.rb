d3f7d7517ac5be29bc14a99c9f76b95ccc999347
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-06 18:58:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,7 @@ second_number = 2
 
 sum = first_number + second_number
 
-difference = second_number - first_number
+difference = first_number - second_number
 
 product = first_number * second_number
 
